e80398ecdaa0fac50ce12851e192644eaba52db9
Robert_Ordis <kobaken.robert@gmail.com>
2018-09-28 18:41:01 +0900

    [実験準備]コンフィグ可能パラメータに「マップ」をつける試み。あと概念としてセクションとドメインを分離させる。

--- AnnotatedConfig/src/info/ro/gadget/configurator/core/AnnotatedConfig.java
+++ AnnotatedConfig/src/info/ro/gadget/configurator/core/AnnotatedConfig.java
@@ -7,6 +7,7 @@ public abstract class AnnotatedConfig {
 	//読み込みに使ったリーダー
 	//読み込みに使った元ファイル（セクションなど、サブセットの場合は元ファイル。文字列直接の場合はnull）
 	String			__section__ 	= null;	//読み込んだセクション（フルパス。未指定の場合はnull）
+	String[]		__domains__		= null;	//ドメイン指定。
 	String			__src__			= null;	//読み込みに使った元ファイル（サブセットなど、文字列を直接読んだ場合はnull）
 	ConfigReader	__reader__		= null;	//このコンフィグを読み込むのに使ったリーダ
 	
@@ -32,5 +33,31 @@ public abstract class AnnotatedConfig {
 	}
 	public void setReader(ConfigReader arg) {
 		this.__reader__ = arg;
+	}
+	public String getDomainString(String splitter) {
+		if(this.__domains__ == null) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.__domains__[0]);
+		for(int i = 1; i < this.__domains__.length; i++) {
+			if(!this.__domains__[i-1].equals("")) {
+				sb.append(splitter);
+			}
+			sb.append(this.__domains__[i]);
+		}
+		return sb.toString();
+	}
+	
+	public void setDomainPath(AnnotatedConfig parent, String mine) {
+		int parentLen = 0;
+		if(parent != null && parent.__domains__ != null) {
+			parentLen = parent.__domains__.length;
+		}
+		this.__domains__ = new String[parentLen];
+		for(int i = 0; i < parentLen; i++) {
+			this.__domains__[i] = parent.__domains__[i];
+		}
+		this.__domains__[parentLen] = mine;
 	}
 }
