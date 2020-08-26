818dcf5f324832d0b1acb408914edfca655850d9
Robert_Ordis <kobaken.robert@gmail.com>
2020-08-26 15:32:17 +0900

    ・変なスペースが入ってたので削除

--- AnnotatedConfig/src/main/java/info/ro/gadget/atsignconfig/core/instance/deserializer/DeserializerStore.java
+++ AnnotatedConfig/src/main/java/info/ro/gadget/atsignconfig/core/instance/deserializer/DeserializerStore.java
@@ -10,20 +10,20 @@ public class DeserializerStore {
 	
 	Map<Class<?>, AcDeserializer> map = new HashMap<Class<?>, AcDeserializer>();
 	
-	 private static final Map<Class<?>, Class<?>> PRIMITIVES_TO_WRAPPERS = new HashMap<Class<?>, Class<?>>();
-	 static {
-		 PRIMITIVES_TO_WRAPPERS.put(boolean.class, Boolean.class);
-		 PRIMITIVES_TO_WRAPPERS.put(byte.class, Byte.class);
-		 PRIMITIVES_TO_WRAPPERS.put(char.class, Character.class);
-		 PRIMITIVES_TO_WRAPPERS.put(double.class, Double.class);
-		 PRIMITIVES_TO_WRAPPERS.put(float.class, Float.class);
-		 PRIMITIVES_TO_WRAPPERS.put(int.class, Integer.class);
-		 PRIMITIVES_TO_WRAPPERS.put(long.class, Long.class);
-		 PRIMITIVES_TO_WRAPPERS.put(short.class, Short.class);
-		 PRIMITIVES_TO_WRAPPERS.put(void.class, Void.class);
-	 }
+	private static final Map<Class<?>, Class<?>> PRIMITIVES_TO_WRAPPERS = new HashMap<Class<?>, Class<?>>();
+	static {
+		PRIMITIVES_TO_WRAPPERS.put(boolean.class, Boolean.class);
+		PRIMITIVES_TO_WRAPPERS.put(byte.class, Byte.class);
+		PRIMITIVES_TO_WRAPPERS.put(char.class, Character.class);
+		PRIMITIVES_TO_WRAPPERS.put(double.class, Double.class);
+		PRIMITIVES_TO_WRAPPERS.put(float.class, Float.class);
+		PRIMITIVES_TO_WRAPPERS.put(int.class, Integer.class);
+		PRIMITIVES_TO_WRAPPERS.put(long.class, Long.class);
+		PRIMITIVES_TO_WRAPPERS.put(short.class, Short.class);
+		PRIMITIVES_TO_WRAPPERS.put(void.class, Void.class);
+	}
 	
-	 private static long presetLong(String s) {
+	private static long presetLong(String s) {
 		final Pattern hexPattern = Pattern.compile("^(-?)0x([0-9a-fA-F]+)$");
 		int baseNum = 10;
 		s = s.trim();
